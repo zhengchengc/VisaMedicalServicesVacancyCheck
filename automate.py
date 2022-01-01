21bf49f7fecbedf43c04fa26e8e05eb9785ca809
zhengchengc <zhengchengc.swe@gmail.com>
2022-01-01 23:08:29 +1100

    first version done

---
+++ automate.py
@@ -0,0 +1,59 @@
+from datetime import datetime, timedelta
+
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+
+currentTime = datetime.strptime("18/2/2022", '%d/%m/%Y')
+print("The current schedule date is " + currentTime.strftime("%d/%m/%Y"))
+newTime = currentTime
+
+n = 1000
+while n > 1:
+    automateDriver = webdriver.Firefox()
+    automateDriver.get("https://bmvs.onlineappointmentscheduling.net.au/oasis/")  # put here the address of your page
+    newIndividualBookingButton = automateDriver.find_element(By.ID, 'ContentPlaceHolder1_btnInd')
+    # print(newIndividualBookingButton.get_attribute("class"))
+    newIndividualBookingButton.click()
+
+    postCodeInput = automateDriver.find_element(By.ID, 'ContentPlaceHolder1_SelectLocation1_txtSuburb')
+    postCodeInput.send_keys('3123')
+    postCodeInput.send_keys(Keys.ENTER)
+
+    automateDriver.implicitly_wait(10)
+
+    suburbSelection = automateDriver.find_element(By.XPATH, '//*[@class="tdlocNameTitle"]')
+    # print(suburbSelection.text)
+    suburbSelection.click()
+
+    # WebDriverWait(automateDriver, 10).until(EC.alert_is_present())
+    # automateDriver.switch_to.alert.accept()
+    nextButton = automateDriver.find_element(By.XPATH, '//*[@id="ContentPlaceHolder1_btnCont"]')
+    nextButton.click()
+
+    automateDriver.find_element(By.ID, 'chkClass1_489').click()
+    automateDriver.find_element(By.ID, 'chkClass1_492').click()
+
+    nextButton = automateDriver.find_element(By.XPATH, '//*[@id="ContentPlaceHolder1_btnCont"]')
+    nextButton.click()
+
+    newTimeRaw = automateDriver.find_element(By.ID, 'ContentPlaceHolder1_SelectTime1_txtAppDate').get_attribute("value")
+    if newTimeRaw:
+        newTime = datetime.strptime(newTimeRaw, '%d/%m/%Y')
+        print(newTime)
+    else:
+        newTime = currentTime + timedelta(days=1)
+
+    print("<<< new time " + newTime.strftime("%d/%m/%Y") + " Melbourne")
+
+    if newTime >= currentTime:
+        print("😭")
+    else:
+        print("There is a earlier time, please secure the spot soon")
+
+    automateDriver.implicitly_wait(30)
+    automateDriver.close()
+    n -= 1
